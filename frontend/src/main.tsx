c6a75140d9b359fff1901637710ef458fa8eaa18
Robby Smith <codenut33@gmail.com>
2026-06-27 04:42:31 -0500

    scss setup, cleanup app.css into individual files

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/base.css";
-import "./styles/app.css";
+import "./styles/tailwind.css";
+import "./styles/app.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
